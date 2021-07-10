a531215398b879f693547bac66b82a6c10fba90b
Max Frisson <max.chebonenko@gmail.com>
2021-07-10 22:50:13 +0300

    add MiniPizzaBlock

---
+++ src/components/MiniPizzaBlock/MiniPizzaBlock.jsx
@@ -0,0 +1,13 @@
+export const MiniPizzaBlock = () => {
+  return (
+    <div>
+      <ul>
+        <li>Пепперони</li>
+        <li>Мясная</li>
+        <li>Сырная</li>
+        <li>Веган плюс</li>
+        <li>7 морей</li>
+      </ul>
+    </div>
+  );
+};
