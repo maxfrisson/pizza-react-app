15130dc86b907a58c6ec59d992750bdf2f6073e9
Max Frisson <max.chebonenko@gmail.com>
2021-07-17 23:59:25 +0300

    add newItems in cart reducer

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -6,17 +6,20 @@ const initialState = {
 
 const cart = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_PIZZA_TO_CART":
-      return {
-        ...state,
-        items: {
+    case "ADD_PIZZA_TO_CART":{
+      const newItems = {
           ...state.items,
           [action.payload.id]: !state.items[action.payload.id]
             ? [action.payload]
             : [...state.items[action.payload.id], action.payload],
-        },
-      };
+        }
 
+
+      return {
+        ...state,
+        items: newItems,
+      };
+}
     default:
       return state;
   }
